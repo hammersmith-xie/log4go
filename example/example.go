af63e63989719e0b13543035e26677817b1dec78
ao.xie <ao.xie@baidao.com>
2018-06-13 17:24:41 +0800

    update

---
+++ example/example.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	l4g "github.com/hammersmith-xie/log4go"
+	"time"
+)
+
+func main(){
+	l4g.InitFileLogWriter("gateway","log/")
+	l4g.Finest("songshiqi")
+	l4g.Error("3Oh no!  %d + %d = %d!", 2, 2, 2+2)
+
+	l4g.Info("all about songshiqi")
+	time.Sleep(1*time.Second)
+}
